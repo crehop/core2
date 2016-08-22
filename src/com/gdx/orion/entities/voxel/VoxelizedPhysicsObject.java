6aed6e49e50d76d1210e5afd9d318c0cd29a11b5
Crehop <crehop2@gmail.com>
2016-08-21 18:13:52 -0700

    Working on physics algorithm

--- src/com/gdx/orion/entities/voxel/VoxelizedPhysicsObject.java
+++ src/com/gdx/orion/entities/voxel/VoxelizedPhysicsObject.java
@@ -2,6 +2,7 @@ package com.gdx.orion.entities.voxel;
 
 import java.util.ArrayList;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.gdx.orion.handlers.VoxelizedPhysicsHandler;
 
@@ -12,6 +13,7 @@ public class VoxelizedPhysicsObject {
 	private float[] object;
 	private ArrayList<Float> floatList = new ArrayList<Float>();
 	private int count = 0;
+	private Vector2 stored = new Vector2 (-1,-1);
 	private float voxelSize = .25f;
 	
 	public VoxelizedPhysicsObject(Voxel[][] voxels, World world){
@@ -23,16 +25,20 @@ public class VoxelizedPhysicsObject {
 			floatList.add(voxelSize);
 			floatList.add(voxelSize);
 			floatList.add(voxelSize);
-			floatList.add(-voxelSize);
 			floatList.add(voxelSize);
 			floatList.add(-voxelSize);
 			floatList.add(-voxelSize);
+			floatList.add(-voxelSize);
 		}
+		
 		for(int x = 0; x < this.getVoxelArray().length; x++){
 			for(int y = 0; y < this.getVoxelArray()[x].length; y++){
-				if(voxels[x][y].type != VoxelType.AIR){
-					floatList.add(x * voxelSize);
-					floatList.add(y * voxelSize);
+				if(this.voxelArray[x][y].getType() != VoxelType.AIR){
+					if(this.getVoxelWest(x, y) == VoxelType.AIR){
+						floatList.add(-voxelSize * (x + 1));
+						floatList.add(voxelSize * (voxelArray[0].length - y));
+					}
+					if(this.getVoxelEast(x, y).type)
 				}
 			}
 		}	
@@ -73,4 +79,53 @@ public class VoxelizedPhysicsObject {
 		this.world = world;
 	}
 	
+	public VoxelType getVoxelNorth(int currentX, int currentY){
+		if(currentY != 0){
+			return voxelArray[currentX][currentY-1].type;
+		}else{
+			return VoxelType.AIR;
+		}
+	}
+	public VoxelType getVoxelSouth(int currentX, int currentY){
+		if(currentY != voxelArray[0].length - 1){
+			return voxelArray[currentX][currentY+1].type;
+		}
+		return VoxelType.AIR;
+	}
+	public VoxelType getVoxelEast(int currentX, int currentY){
+		if(currentX != voxelArray.length - 1){
+			return voxelArray[currentX+1][currentY].type;
+		}
+		return VoxelType.AIR;
+	}
+	public VoxelType getVoxelWest(int currentX, int currentY){
+		if(currentX != 0){
+			return voxelArray[currentX-1][currentY].type;
+		}
+		return VoxelType.AIR;
+	}
+	public VoxelType getVoxelNorthWest(int currentX, int currentY){
+		if(currentY != 0 && currentX != 0){
+			return voxelArray[currentX-1][currentY - 1].type;
+		}
+		return VoxelType.AIR;
+	}
+	public VoxelType getVoxelSouthWest(int currentX, int currentY){
+		if(currentX != 0 && currentY != voxelArray[0].length - 1){
+			return voxelArray[currentX-1][currentY + 1].type;
+		}
+		return VoxelType.AIR;
+	}
+	public VoxelType getVoxelNorthEast(int currentX, int currentY){
+		if(currentY != 0 && currentX != voxelArray.length - 1){
+			return voxelArray[currentX + 1][currentY - 1].type;
+		}
+		return VoxelType.AIR;
+	}
+	public VoxelType getVoxelSouthEast(int currentX, int currentY){
+		if(currentY != voxelArray[0].length - 1 && currentX != voxelArray.length - 1){
+			return voxelArray[currentX-1][currentY- 1].type;
+		}
+		return VoxelType.AIR;
+	}
 }
