624282adedeb21ca26d99364bb413dc6b10ae85c
Dolphindalt <dolphindalt@gmail.com>
2016-09-11 16:55:43 -0700

    Fixing Voxel Class
    My push merged for some reason.

--- src/com/gdx/orion/entities/voxel/Voxel.java
+++ src/com/gdx/orion/entities/voxel/Voxel.java
@@ -3,12 +3,14 @@ package com.gdx.orion.entities.voxel;
 public abstract class Voxel {
 	
 	public static final float voxelSize = 0.25f;
+	public static final float voxelGap = 0.256f;
 	
 	public VoxelType type;
 	
 	protected int durability;
 	protected float density;
 	protected float absorbtion;
+	protected float friction;
 	
 	protected boolean breakable;
 	
